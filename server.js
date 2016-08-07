989af2bc42429efb3b1607d84c84107823744609
web-node <projectwebdcb@gmail.com>
2016-08-07 21:31:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();   // App an instance of express
 
+app.set('port',process.env.PORT || 5000);
 
-app.listen(3000);
+app.listen(app.get('port'),function () {
+    console.log("Server started and listening at port " + app.get('port'));
+});
